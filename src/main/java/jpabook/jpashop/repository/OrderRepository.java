c32e80751bae7bc00b895bd89fde66f3fa46bd92
sujnkim <blackout3210@naver.com>
2024-02-17 02:58:03 +0900

    feat: JPQL을 이용한 주문 검색 기능 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -27,9 +27,54 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    /**
+     * JPQL 사용
+     * 쿼리를 문자로 표현하기 어렵고 버그 발생 가능성이 높음
+     */
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
 
     /**
      * JPA Criteria 사용
+     * 실무에서 사용하기 복잡 -> 대신 Querydsl을 사용하도록 하자
      */
     public List<Order> findAllByCriteria(OrderSearch orderSearch) {
 
